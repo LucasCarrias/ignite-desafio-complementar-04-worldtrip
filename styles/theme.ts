61567f0760ae126caa8bf5dc25f6d8a8e7f0a1e2
Lucas Carrias <lucascarrias@outlook.com>
2021-06-14 20:34:20 -0300

    Setup typescript and chakra ui

---
+++ styles/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      "900": "#1C1401",
+      "500": '#FFBA08'
+    },
+    gray: {
+      "600": "#47585B",
+      "500": "#999",
+      "100": "#DADADA",
+      "50": "#F5F8FA"
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.50',
+        color: 'gray.500'
+      }
+    }
+  }
+});
